dff494e3e9a574e6e2390b7587b62ecd4cecb974
Vikki <vikki.read@gmail.com>
2013-06-16 21:07:43 +0100

    Minor refactor / general tidy

--- scripts/almost_everything.js
+++ scripts/almost_everything.js
@@ -1,166 +1,163 @@
-  function parseRanges(rangeStrArray){
-    var objectRegexDets = {
-      re: /(x|y\d?)range \[([\d.]+)\:([\d.]+)\]/,
-      groupObjMapping: ['', 'name', 'from', 'to']
-    };
+function parseRanges(rangeStrArray){
+  var objectRegexDets = {
+    re: /(x|y\d?)range \[([\d.]+)\:([\d.]+)\]/,
+    groupObjMapping: ['', 'name', 'from', 'to']
+  };
 
-    return genericMatcher(rangeStrArray, objectRegexDets);
-  }
+  return genericMatcher(rangeStrArray, objectRegexDets);
+}
 
-  function parseAxisLabels(labelStrArray){
-    var objectRegexDets = {
-      re: /(x|y\d+)label "(.*)"/,
-      groupObjMapping: ['', 'axis', 'label']
-    };
+function parseAxisLabels(labelStrArray){
+  var objectRegexDets = {
+    re: /(x|y\d+)label "(.*)"/,
+    groupObjMapping: ['', 'axis', 'label']
+  };
 
-    return genericMatcher(labelStrArray, objectRegexDets);
-  }
+  return genericMatcher(labelStrArray, objectRegexDets);
+}
 
-  function parseLabels(labelStrArray){
-    var objectRegexDets = {
-      re: /set label "([^"]*)" at ([\d.]+),([\d.]+) (textcolor rgb "([\w|#]+)")?/,
-      groupObjMapping: ['', 'label', 'from', 'to', '', 'hexColor']
-    };
+function parseLabels(labelStrArray){
+  var objectRegexDets = {
+    re: /set label "([^"]*)" at ([\d.]+),([\d.]+) (textcolor rgb "([\w|#]+)")?/,
+    groupObjMapping: ['', 'label', 'from', 'to', '', 'hexColor']
+  };
 
-    return genericMatcher(labelStrArray, objectRegexDets);
-  }
+  return genericMatcher(labelStrArray, objectRegexDets);
+}
 
-  function parseDunno(dunnoStrArray){
-    var objectRegexDets = {
-      re: /([\d.]+) ([\d.]+)/,
-      groupObjMapping: ['', 'xValue', 'yValue']
-    };
+function parseDunno(dunnoStrArray){
+  var objectRegexDets = {
+    re: /([\d.]+) ([\d.]+)/,
+    groupObjMapping: ['', 'xValue', 'yValue']
+  };
 
-    return genericMatcher(dunnoStrArray, objectRegexDets);
-  }
+  return genericMatcher(dunnoStrArray, objectRegexDets);
+}
 
-  function genericMatcher(input, outputDets){
-     return input.map(function(bla){
-        var match = bla.match(outputDets.re);
-        var output = {};
+function genericMatcher(input, outputDets){
+   return input.map(function(bla){
+      var match  = bla.match(outputDets.re),
+          output = {},
+          name;
 
-        for (var i = 0; i < outputDets.groupObjMapping.length; i++) {
-           var name = outputDets.groupObjMapping[i];
-           if (typeof name == 'undefined' || name.length < 1) {
-              continue;
-           }
-           output[name] = match[i];
-        }
-
-        return output;
-     });
-  }
-
-  function parseObjects(objectStrArray){
-    var objectRegexDets = {
-      re: /(\d+) rect from ([\d.]+), ([\d.]+) to ([\d.]+), ([\d.]+) fc rgb "(#[\w]+)"/,
-      groupObjMapping: ['', 'id', 'fromX', 'fromY', 'toX', 'toY', 'color']
-    };
-
-    return genericMatcher(objectStrArray, objectRegexDets);
-  }
-
-
-  function parseTics(ticStrArray){
-     return ticStrArray.map(function(bla){
-          // also should prolly share the regex with the partitioner thingy
-          // may want to filter out the empty ones earlier?
-          // ERGH refactor
-          var axis, 
-              tics,
-              foo = bla.match(/set (x|y)tics out nomirror( \((.+)\))?/);
-
-          axis = foo[1];
-
-          if (!foo || !foo[2] || !foo[3]){
-            return {
-               axis: axis,
-               tics: []
-            };
-          }
-
-          tics = foo[3].split(',')
-                      .map(function(i){
-                        var match = i.match(/"(.+)" ([\d.]+)/);
-                        return {
-                          title: match[1], 
-                          position: match[2]
-                        };
-                      });
-
-         return {
-           axis: axis,
-           tics: tics
-         };
-      }); 
-  }
-
-   // sweet jesus, rename when more awake
-  function findPartsOfOutput(output){
-      var lines = output.split('\n');
-
-       // tmp global ofr messing about purposes
-      miao = _.chain(lines)
-                 .reject(function(line) {
-                   return line.match(/^((unset key)|(plot)|(set style)|(e)|$)/);
-                  })
-                 .groupBy(function(line) {
-                  // OK the if statement is a little ropey but its still nicer than before.. 
-                  if (line.match(/^set object/)) {
-                    return 'objects';
-                  } else if (line.match(/^set label/)) {
-                    return 'labels';
-                  } else if (line.match(/^set (x|y)\d?tics/)) {
-                    return 'tics';
-                  } else if (line.match(/^set (x|y)\d?range/)) {
-                    return 'ranges';
-                  } else if (line.match(/^set (x|y)\d?label/)) {
-                    return 'axisLabels';
-                  } else {
-                    return 'dunno';
-                  }
-                })
-
-        out =  miao.value();
-
-        // moar tmp globally goodness
-        out.ranges = parseRanges(out.ranges);
-        out.axisLabels = parseAxisLabels(out.axisLabels);
-        out.labels = parseLabels(out.labels);
-        out.dunno = parseDunno(out.dunno);
-        out.tics = parseTics(out.tics);
-        out.objects = parseObjects(out.objects);
-  }
-
-  function handleFileSelect(evt) {
-    var files = evt.target.files; // FileList object
-
-    // Loop through the FileList and render image files as thumbnails.
-    for (var i = 0, f; f = files[i]; i++) {
-
-      // Only process text files. - could warn instead and try to parse anyway?
-      if (!f.type.match('text.*')) {
-      	console.log('dude thats not a text file its a ' + f.type + ' file');
-        continue;
+      for (var i = 0; i < outputDets.groupObjMapping.length; i++) {
+         name = outputDets.groupObjMapping[i];
+         if (typeof name == 'undefined' || name.length < 1) {
+            continue;
+         }
+         output[name] = match[i];
       }
 
-      var reader = new FileReader();
+      return output;
+   });
+}
 
-      reader.onload = (function(theFile) {
-        return function(e) {
-        	var contents = e.target.result;
-        	//console.log(contents);
-            findPartsOfOutput(contents);
+function parseObjects(objectStrArray){
+  var objectRegexDets = {
+    re: /(\d+) rect from ([\d.]+), ([\d.]+) to ([\d.]+), ([\d.]+) fc rgb "(#[\w]+)"/,
+    groupObjMapping: ['', 'id', 'fromX', 'fromY', 'toX', 'toY', 'color']
+  };
 
-            var data = [24, 57, 98, 12];
-            var chart = setupChart();
-            drawDets(chart, out);
-        };
-      })(f);
+  return genericMatcher(objectStrArray, objectRegexDets);
+}
 
-      // Read in the image file as a data URL.
-      reader.readAsText(f);
+
+function parseTics(ticStrArray){
+   return ticStrArray.map(function(bla){
+        // also should prolly share the regex with the partitioner thingy
+        // may want to filter out the empty ones earlier?
+        // ERGH refactor
+        var axis, 
+            tics,
+            foo = bla.match(/set (x|y)tics out nomirror( \((.+)\))?/);
+
+        axis = foo[1];
+
+        if (!foo || !foo[2] || !foo[3]){
+          return {
+             axis: axis,
+             tics: []
+          };
+        }
+
+        tics = foo[3].split(',')
+                    .map(function(i){
+                      var match = i.match(/"(.+)" ([\d.]+)/);
+                      return {
+                        title: match[1], 
+                        position: match[2]
+                      };
+                    });
+
+       return {
+         axis: axis,
+         tics: tics
+       };
+    }); 
+}
+
+ // sweet jesus, rename when more awake
+function findPartsOfOutput(output){
+    var lines = output.split('\n'),   
+        output =  _.chain(lines)
+                   .reject(function(line) {
+                     return line.match(/^((unset key)|(plot)|(set style)|(e)|$)/);
+                    })
+                   .groupBy(function(line) {
+                    // OK the if statement is a little ropey but its still nicer than before.. 
+                    if (line.match(/^set object/)) {
+                      return 'objects';
+                    } else if (line.match(/^set label/)) {
+                      return 'labels';
+                    } else if (line.match(/^set (x|y)\d?tics/)) {
+                      return 'tics';
+                    } else if (line.match(/^set (x|y)\d?range/)) {
+                      return 'ranges';
+                    } else if (line.match(/^set (x|y)\d?label/)) {
+                      return 'axisLabels';
+                    } else {
+                      return 'dunno';
+                    }
+                  })
+                  .value();
+
+      output.ranges = parseRanges(output.ranges);
+      output.axisLabels = parseAxisLabels(output.axisLabels);
+      output.labels = parseLabels(output.labels);
+      output.dunno = parseDunno(output.dunno);
+      output.tics = parseTics(output.tics);
+      output.objects = parseObjects(output.objects);
+
+      // tmp global ofr messing about purposes
+      out = output;
+}
+
+function handleFileSelect(evt) {
+  var files = evt.target.files,
+      reader;
+
+  for (var i = 0, f; f = files[i]; i++) {
+
+    // Only process text files. - could warn instead and try to parse anyway?
+    if (!f.type.match('text.*')) {
+      console.log('dude thats not a text file its a ' + f.type + ' file');
+      continue;
     }
-  }
 
-  document.getElementById('files').addEventListener('change', handleFileSelect, false);
+    reader = new FileReader();
+
+    reader.onload = (function(theFile) {
+      return function(e) {
+        var contents = e.target.result;
+          findPartsOfOutput(contents);
+
+          setupChart();
+          drawDets(out);
+      };
+    })(f);
+
+    reader.readAsText(f);
+  }
+}
+
+document.getElementById('files').addEventListener('change', handleFileSelect, false);
--- scripts/d3_stuff.js
+++ scripts/d3_stuff.js
@@ -1,148 +1,144 @@
 // yeah i know. TODO refactor
-chartHeight = 500;
-chartHeightIncTrimmings = 600;
-bottomChartHeight = 200 + 300 //confusion;
-chartWidth  = 1000;
-yMax = 23.5;
+var chartHeight = 500,
+    chartHeightIncTrimmings = 600,
+    bottomChartHeight = 200 + 300, //confusion;
+    chartWidth  = 1000;
 
-function setupChart() {    
-	var chart = d3.select("body")
-	              .append("svg")
-	              .attr("class", "chart")
-	              .attr("width",  chartWidth + 300)
-	              .attr("height", chartHeightIncTrimmings)
-	              .attr("style", "width: " + 1300 + "px; height: " + chartHeightIncTrimmings +"px;");
+function setupChart() {
+  d3.select("body")
+    .append("svg")
+    .attr("class", "chart")
+    .attr("width",  chartWidth + 300)
+    .attr("height", chartHeightIncTrimmings)
+    .attr("style", "width: " + 1300 + "px; height: " + chartHeightIncTrimmings +"px;");
 
-	return chart;
+
+  d3.select("svg")
+    .append("g")
+    .attr('id', 'graphContents')
+    .attr("transform", "translate(0,0)");
 }
 
-function drawDets(chart, data) {
+function drawDets(data) {
   xScale = d3.scale
           .linear()
-          .domain([1.010355,20494.654359999982])
+          .domain([out.ranges[1].from, out.ranges[1].to])
           .range([0, chartWidth]);
 
   yScale = d3.scale
              .linear()
-             .domain([0,yMax])
+             .domain([out.ranges[0].from, out.ranges[0].to])
              .range([0, chartHeight]);
 
-drawTicks(chart, data.tics[1].tics);
-
-var chart = d3.select("svg")
-              .append("g")
-              .attr('id', 'graphContents')
-              .attr("transform", "translate(0,0)");
-  
-  drawObjects(chart, data.objects);
-
+  drawTicks(data.tics[1].tics);
+  drawObjects(data.objects);
   drawDunno(data.dunno);
 }
-
 
 // i wanted to call this flip-reverse it sooo bad :P
 // paramName is also a bit lame, reconsider
 function flipYForD3Axis(paramName, e){
-	e[paramName] = yMax - e[paramName];
+  e[paramName] = out.ranges[0].to - e[paramName];
+  return e;
+}
+
+function drawObjects(data) {
+  function calcWidthAndHeight(e){
+    var width,
+        height,
+        objectWrapper;
+
+    width = e.toX - e.fromX; 
+    e.width = width >= 0 ? width.toFixed(2) : 0;
+
+    height = e.toY - e.fromY;
+    e.height = height >= 0 ? height.toFixed(2) : 0;
+
     return e;
+  }
+
+  data = data.map(calcWidthAndHeight)
+             .map(flipYForD3Axis.bind(null, 'fromY'));
+
+  objectWrapper = d3.select("#graphContents")
+                    .append("g")
+                    .attr('id', 'objects')
+                    .attr('transform', 'translate(0, -15)');
+
+  objectWrapper.selectAll("rect")
+               .data(data) 
+               .enter()
+               .append("rect")
+               .attr("id",     function(d, i) { return d.id; })
+               .attr("width",  function(d, i) { return xScale(d.width).toFixed(2); })
+               .attr("x",      function(d, i) { return xScale(d.fromX).toFixed(2); })
+               .attr("height", function(d, i) { return yScale(d.height).toFixed(2); })
+               .attr("y",      function(d, i) { return yScale(d.fromY).toFixed(2); })
+               .attr("fill",   function(d, i) { return d.color; });
 }
 
-function drawObjects(chart, data) {
-    data = data.map(function calcWidthAndHeight(e){
-    	              var width,
-    	                  height;
+function drawTicks(data){
+  var xAxisContainer,
+      yAxisContainer,
+      xAxis,
+      yAxis;
 
-    	              width = e.toX - e.fromX; 
-                      e.width = width >= 0 ? width.toFixed(2) : 0;
+  data = data.map(flipYForD3Axis.bind(null, 'position'));
 
-                      height = e.toY - e.fromY;
-                      e.height = height >= 0 ? height.toFixed(2) : 0;
+  yAxisContainer = d3.select("svg")
+                     .append("g")
+                     .attr('id', 'yAxis')
+                     .attr("class", "axis")
+                     .attr("transform", "translate(0,0)");
 
-                      return e;
-                    })
-                .map(flipYForD3Axis.bind(null, 'fromY'));
+  yAxis = d3.svg.axis()
+                .scale(yScale)
+                .orient("left")
+                .tickValues(_.map(out.tics[1].tics, function(e) {return e.position}))
+                .tickFormat(function getTickTitleFromPosition(p){
+                   var ticValue = _.find(out.tics[1].tics, 
+                                         function(o, i) { 
+                                           return o.position === p;
+                                         });
+                   return ticValue ? ticValue.title : '';
+                });
+  yAxisContainer.call(yAxis);
 
-     var chart = d3.select("#graphContents")
-	               .append("g")
-	               .attr('id', 'objects')
-                 .attr('transform', 'translate(0, -15)');
+  xAxisContainer = d3.select("svg")
+                     .append("g")
+                     .attr('id', 'xAxis')
+                     .attr("class", "axis")
+                     .attr("transform", "translate(0,500)");
 
-	chart.selectAll("rect")
-		 .data(data) 
-		 .enter()
-		 .append("rect")
-         .attr("id",     function(d, i) { return d.id; })
-		 .attr("width",  function(d, i) { return xScale(d.width).toFixed(2); })
-		 .attr("x",      function(d, i) { return xScale(d.fromX).toFixed(2); })
-		 .attr("height", function(d, i) { return yScale(d.height).toFixed(2); })
-		 .attr("y",      function(d, i) { return yScale(d.fromY).toFixed(2); })
-		 .attr("fill",   function(d, i) { return d.color; });
-
-	return chart;
-}
-
-// ticks are a little high for now because objects only take up half the y-axis (rest are the dunno objects :P)
-// POSITION IS JUST AN ORDERING THING, or % ITS NOT AN ACTUAL POSITION, well it kinda it once you have a ratio..
-// i think i r doing it wrong - need to use an axis http://www.dashingd3js.com/d3js-axes
-function drawTicks(wibble, data){
-	data = data.map(flipYForD3Axis.bind(null, 'position'));
-
-    var chart = d3.select("svg")
-	                .append("g")
-	                .attr('id', 'yAxis')
-                  .attr("class", "axis")
-                  .attr("transform", "translate(0,0)");
-
-      var yAxis = d3.svg.axis()
-                        .scale(yScale)
-                        .orient("left")
-                        .tickValues(_.map(out.tics[1].tics, function(e) {return e.position}))
-                        .tickFormat(function(d){
-                           var ticValue = _.find(out.tics[1].tics, function(o, i) { return o.position === d;});
-                           return ticValue ? ticValue.title : '';
-                        });
-      chart.call(yAxis);
-
-
-      chart = d3.select("svg")
-                .append("g")
-                .attr('id', 'xAxis')
-                .attr("class", "axis")
-                .attr("transform", "translate(0,500)");
-
-      var xAxis = d3.svg.axis()
-                        .scale(xScale);
-      xAxis.ticks(5);  // needed? doesn't seem to be in the input file but its how the output looks
-      chart.call(xAxis);
+  xAxis = d3.svg.axis()
+                .scale(xScale);
+  xAxis.ticks(5);  // needed? doesn't seem to be in the input file but its how the output looks
+  xAxisContainer.call(xAxis);
 }
 
 function drawDunno(data) {
-	data = data.map(function flipYForD3Axis( e){
-			//e.yValue = 128.476035 - e.yValue;
-			return e;
-		});
+  // think this is right but there's way too much magic (numbers), need to make it more flexible, scaleable
+  var height = 250,
+      container,
+      yScale2;
 
-// think this is right but there's way too much magic (numbers), need to make it more flexible, scaleable
-// and also learn how svg works in terms of layering :P
-  var h = 250;
+  yScale2 = d3.scale
+              .linear()
+              .domain([out.ranges[2].from, out.ranges[2].to])
+              .range([0, height]);
 
-		yScale2 = d3.scale
-	           .linear()
-	           .domain([0,128.476035])
-	           .range([0, h]);
+  container = d3.select("#graphContents")
+                .append("g")
+                .attr('id', 'dunno')
+                .attr('height', height + 'px');
 
-    var chart = d3.select("#graphContents")
-               .append("g")
-               .attr('id', 'dunno')
-               .attr('height', h + 'px');
-
-    chart.selectAll("line")
-         .data(data)
-         .enter().append("line")
-	         .attr("x1", function(d, i) { return xScale(d.xValue).toFixed(2); })
-	         .attr("x2", function(d, i) { return xScale(d.xValue).toFixed(2); })
-	         .attr("y1", function(d, i) { return yScale2(h).toFixed(2); })
-	         .attr("y2", function(d, i) { return yScale2(h - d.yValue).toFixed(2); })
-	         .style("stroke", "#000");
-	}
+  container.selectAll("line")
+           .data(data)
+           .enter().append("line")
+           .attr("x1", function(d, i) { return xScale(d.xValue).toFixed(2); })
+           .attr("x2", function(d, i) { return xScale(d.xValue).toFixed(2); })
+           .attr("y1", function(d, i) { return yScale2(height).toFixed(2); })
+           .attr("y2", function(d, i) { return yScale2(height - d.yValue).toFixed(2); })
+           .style("stroke", "#000");
+  }
 
